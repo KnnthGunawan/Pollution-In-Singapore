62bb5dfd278126479ca7bae7f01626cf403041b7
Kenneth <knnthgunawan@gmail.com>
2021-08-19 13:24:23 +0800

    added button and image

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ function toggleText() {
         var text2 = document.getElementById("pollution-text");
         text2.style.display = "block";
     }, 1500);
+    setTimeout(function(){
+        var text3 = document.getElementById("next_button");
+        text3.style.display = "block";
+    }, 3000);
+}
+
+function toggleText2() {
+    var text4 = document.getElementById("picture");
+    text4.style.display = "block";
 }
