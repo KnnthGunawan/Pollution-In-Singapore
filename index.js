31a59c4363f0e8062bac96ae7053823de453cd34
Kenneth <knnthgunawan@gmail.com>
2021-08-28 18:07:34 +0800

    added Google Chart and other stuff

--- index.js
+++ index.js
@@ -18,4 +18,44 @@ function toggleText2() {
         var text5 = document.getElementById("9.09");
         text5.style.display = "block";
     }, 1500)
+    setTimeout(function(){
+        var graph1 = document.getElementById("chart_div");
+        graph1.style.display = "block";
+    }, 3000)
+    setTimeout(function(){
+        var text6 = document.getElementById("211");
+        text6.style.display = "block";
+    }, 3100)
+}
+
+/* Google Charts Stuff*/
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(drawBasic);
+
+function drawBasic() {
+
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'X');
+    data.addColumn('number', 'Value');
+
+    data.addRows([
+        ["2009", 177], ["2010", 185],  ["2011", 193],  ["2012", 192],  ["2013", 191],  ["2014", 191],
+        ["2015", 203],  ["2016", 217],  ["2017", 229],  ["2018", 225],  ["2019", 217], ["2020", 211]
+    ]);
+
+    var options = {
+        title: "Singapore's C02 Emissions from Fossil-fuels",
+        width: 900,
+        height: 450,
+        hAxis: {
+          title: 'Year'
+        },
+        vAxis: {
+          title: 'C02 Emissions (million tonnes)'
+        }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
 }
