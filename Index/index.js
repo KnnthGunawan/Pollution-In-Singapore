555dab8f492d0d9f8407f7712db65a63b5e18800
Kenneth <knnthgunawan@gmail.com>
2021-09-01 21:25:03 +0800

    Changed Google Charts to different website, and started page 2
    New charts have watermaker top left, but it's more convenient and easy to implement

--- Index/index.js
+++ Index/index.js
@@ -19,16 +19,20 @@ function toggleText2() {
         text5.style.display = "block";
     }, 1500)
     setTimeout(function(){
-        var graph1 = document.getElementById("chart_div");
+        var graph1 = document.getElementById("chartContainer");
         graph1.style.display = "block";
     }, 3000)
     setTimeout(function(){
         var text6 = document.getElementById("211");
         text6.style.display = "block";
     }, 3100)
+    setTimeout(function(){
+        var text6 = document.getElementById("next_button_2");
+        text6.style.display = "block";
+    }, 3500)
 }
 
-/* Google Charts Stuff*/
+/* Google Charts Stuff
 google.charts.load('current', {packages: ['corechart', 'line']});
 google.charts.setOnLoadCallback(drawBasic);
 
@@ -39,14 +43,14 @@ function drawBasic() {
     data.addColumn('number', 'C02 Emissions');
 
     data.addRows([
-        ["2009", 177], ["2010", 185],  ["2011", 193],  ["2012", 192],  ["2013", 191],  ["2014", 191],
-        ["2015", 203],  ["2016", 217],  ["2017", 229],  ["2018", 225],  ["2019", 217], ["2020", 211]
+        ["1999", 100], ["2000", 107], ["2001", 118], ["2002", 112], ["2003", 104], ["2004", 116], ["2005", 127],
+        ["2006", 140], ["2007", 151], ["2008", 163], ["2009", 177], ["2010", 185],  ["2011", 193],  ["2012", 192],  ["2013", 191],  ["2014", 191], ["2015", 203],  ["2016", 217],  ["2017", 229],  ["2018", 225],  ["2019", 217], ["2020", 211]
     ]);
 
     var options = {
         title: "Singapore's C02 Emissions from Fossil-fuels",
-        width: window.innerWidth,
-        height: (window.innerWidth/2),
+        width: (window.innerWidth-100),
+        height: (window.innerHeight/3),
         hAxis: {
           title: 'Year'
         },
@@ -59,3 +63,6 @@ function drawBasic() {
 
     chart.draw(data, options);
 }
+
+*/
+
