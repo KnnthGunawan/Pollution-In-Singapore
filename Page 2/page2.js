3884658ca48ee4d49e615c3e5f62f51b3412f667
Kenneth <knnthgunawan@gmail.com>
2021-09-11 22:13:52 +0800

    Edited index page

--- Page 2/page2.js
+++ Page 2/page2.js
@@ -1,35 +1,4 @@
-function toggleText() {
-    var text = document.getElementById("question");
-    text.style.display = "block";
-    setTimeout(function(){
-        var text2 = document.getElementById("pollution-text");
-        text2.style.display = "block";
-    }, 1500);
-    setTimeout(function(){
-        var text3 = document.getElementById("next_button");
-        text3.style.display = "block";
-    }, 3000);
-}
 
-function toggleText2() {
-    var text4 = document.getElementById("singapore-text");
-    text4.style.display = "block";
-    setTimeout(function(){
-        var text5 = document.getElementById("9.09");
-        text5.style.display = "block";
-    }, 1500)
-    setTimeout(function(){
-        Chart();
-    }, 3000)
-    setTimeout(function(){
-        var text6 = document.getElementById("211");
-        text6.style.display = "block";
-    }, 3100)
-    setTimeout(function(){
-        var text7 = document.getElementById("next_button_2");
-        text7.style.display = "block";
-    }, 3500)
-}
 
 function Chart () {
     var chart = new CanvasJS.Chart("chartContainer",
@@ -72,6 +41,4 @@ function Chart () {
 
     chart.render();
   }
-
   
-
