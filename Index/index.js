ccc6847167f32c84ffdc7ba848bcb71a39aa254c
Kenneth <knnthgunawan@gmail.com>
2021-09-15 19:27:29 +0800

    Added button_class in Page 2
    Plan on creating the answer appearing animations

--- Index/index.js
+++ Index/index.js
@@ -8,7 +8,7 @@ function toggleText() {
     setTimeout(function(){
         var text3 = document.getElementById("next_button");
         text3.style.display = "block";
-    }, 3000);
+    }, 3500);
 }
 
 function toggleText2() {
@@ -17,61 +17,18 @@ function toggleText2() {
     setTimeout(function(){
         var text5 = document.getElementById("9.09");
         text5.style.display = "block";
-    }, 1500)
-    setTimeout(function(){
-        Chart();
-    }, 3000)
+    }, 2000)
     setTimeout(function(){
         var text6 = document.getElementById("211");
         text6.style.display = "block";
-    }, 3100)
+    }, 4000)
     setTimeout(function(){
         var text7 = document.getElementById("next_button_2");
         text7.style.display = "block";
-    }, 3500)
+    }, 6000)
 }
 
-function Chart () {
-    var chart = new CanvasJS.Chart("chartContainer",
-    {
 
-      title:{
-      text: "Singapore C02 Emissions From Fossil Fuels"
-      },
-       data: [
-      {
-        type: "line",
-
-        dataPoints: [
-        { x: new Date(1999, 00, 1), y: 100 },
-        { x: new Date(2000, 01, 1), y: 107 },
-        { x: new Date(2001, 02, 1), y: 118 },
-        { x: new Date(2002, 03, 1), y: 112 },
-        { x: new Date(2003, 04, 1), y: 104 },
-        { x: new Date(2004, 05, 1), y: 116 },
-        { x: new Date(2005, 06, 1), y: 127 },
-        { x: new Date(2006, 07, 1), y: 140 },
-        { x: new Date(2007, 08, 1), y: 151 },
-        { x: new Date(2008, 09, 1), y: 163 },
-        { x: new Date(2009, 10, 1), y: 177 },
-        { x: new Date(2010, 11, 1), y: 185 },
-        { x: new Date(2011, 11, 1), y: 193 },
-        { x: new Date(2012, 11, 1), y: 192 },
-        { x: new Date(2013, 11, 1), y: 191 },
-        { x: new Date(2014, 11, 1), y: 191 },
-        { x: new Date(2015, 11, 1), y: 203 },
-        { x: new Date(2016, 11, 1), y: 217 },
-        { x: new Date(2017, 11, 1), y: 229 },
-        { x: new Date(2018, 11, 1), y: 225 },
-        { x: new Date(2019, 11, 1), y: 217 },
-        { x: new Date(2020, 11, 1), y: 211 }
-        ]
-      }
-      ]
-    });
-
-    chart.render();
-  }
 
   
 
--- Page 2/page2.js
+++ Page 2/page2.js
@@ -5,19 +5,22 @@ window.onload = function Chart () {
       axisX:{
         title: "Year",
         fontFamily: "montserrat",
+        titleFontSize: 17,
         horizontalAlignment: "center",
         margin: 2
       },
       axisY:{
         title: "C02 Emissions (millions tonnes)",
         fontFamily: "montserrat",
+        titleFontSize: 17,
         horizontalAlignment: "center",
         margin: 2
       },
       title:{
         text: "Singapore C02 Emissions From Fossil Fuels",
         fontFamily: "montserrat",
-        horizontalAlignment: "center",
+        fontSize: 25,
+        horizontalAlignment: "left",
         margin: 2
       },
        data: [
@@ -26,27 +29,27 @@ window.onload = function Chart () {
 
         dataPoints: [
         { x: new Date(1999, 00, 1), y: 100 },
-        { x: new Date(2000, 01, 1), y: 107 },
-        { x: new Date(2001, 02, 1), y: 118 },
-        { x: new Date(2002, 03, 1), y: 112 },
-        { x: new Date(2003, 04, 1), y: 104 },
-        { x: new Date(2004, 05, 1), y: 116 },
-        { x: new Date(2005, 06, 1), y: 127 },
-        { x: new Date(2006, 07, 1), y: 140 },
-        { x: new Date(2007, 08, 1), y: 151 },
-        { x: new Date(2008, 09, 1), y: 163 },
-        { x: new Date(2009, 10, 1), y: 177 },
-        { x: new Date(2010, 11, 1), y: 185 },
-        { x: new Date(2011, 11, 1), y: 193 },
-        { x: new Date(2012, 11, 1), y: 192 },
-        { x: new Date(2013, 11, 1), y: 191 },
-        { x: new Date(2014, 11, 1), y: 191 },
-        { x: new Date(2015, 11, 1), y: 203 },
-        { x: new Date(2016, 11, 1), y: 217 },
-        { x: new Date(2017, 11, 1), y: 229 },
-        { x: new Date(2018, 11, 1), y: 225 },
-        { x: new Date(2019, 11, 1), y: 217 },
-        { x: new Date(2020, 11, 1), y: 211 }
+        { x: new Date(2000, 00, 1), y: 107 },
+        { x: new Date(2001, 00, 1), y: 118 },
+        { x: new Date(2002, 00, 1), y: 112 },
+        { x: new Date(2003, 00, 1), y: 104 },
+        { x: new Date(2004, 00, 1), y: 116 },
+        { x: new Date(2005, 00, 1), y: 127 },
+        { x: new Date(2006, 00, 1), y: 140 },
+        { x: new Date(2007, 00, 1), y: 151 },
+        { x: new Date(2008, 00, 1), y: 163 },
+        { x: new Date(2009, 00, 1), y: 177 },
+        { x: new Date(2010, 00, 1), y: 185 },
+        { x: new Date(2011, 00, 1), y: 193 },
+        { x: new Date(2012, 00, 1), y: 192 },
+        { x: new Date(2013, 00, 1), y: 191 },
+        { x: new Date(2014, 00, 1), y: 191 },
+        { x: new Date(2015, 00, 1), y: 203 },
+        { x: new Date(2016, 00, 1), y: 217 },
+        { x: new Date(2017, 00, 1), y: 229 },
+        { x: new Date(2018, 00, 1), y: 225 },
+        { x: new Date(2019, 00, 1), y: 217 },
+        { x: new Date(2020, 00, 1), y: 211 }
         ]
       }
       ]
