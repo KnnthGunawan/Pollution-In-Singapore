299940ae4fd2661eb0efa53c1ab511a375dc9f1b
Kenneth <knnthgunawan@gmail.com>
2021-10-19 22:14:36 +0800

    added to Page 4

--- Page 4/page4.js
+++ Page 4/page4.js
@@ -2,7 +2,19 @@ function toggleText() {
     var text = document.getElementById("pollution-text");
     text.style.display = "block";
     setTimeout(function(){
-        var text3 = document.getElementById("next_button");
-        text3.style.display = "block";
-    }, 3500);
+        var div1 = document.getElementById("1");
+        div1.style.display = "block";
+    }, 1000);
+    setTimeout(function(){
+        var div2 = document.getElementById("2");
+        div2.style.display = "block";
+    }, 3000);
+    setTimeout(function(){
+        var div3 = document.getElementById("3");
+        div3.style.display = "block";
+    }, 5000);
+    setTimeout(function(){
+        var div4 = document.getElementById("4");
+        div4.style.display = "block";
+    }, 8000);
 }
