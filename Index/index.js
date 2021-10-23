d25c1491e13d76f3bdbdaee84b9010eb1ada48a0
Kenneth <knnthgunawan@gmail.com>
2021-10-23 22:47:28 +0800

    added autoscroll to pg 4

--- Index/index.js
+++ Index/index.js
@@ -4,15 +4,15 @@ function toggleText() {
     setTimeout(function(){
         var text5 = document.getElementById("9.09");
         text5.style.display = "block";
-    }, 2000)
+    }, 1000)
     setTimeout(function(){
         var text6 = document.getElementById("211");
         text6.style.display = "block";
-    }, 4000)
+    }, 2000)
     setTimeout(function(){
         var text7 = document.getElementById("next_button_2");
         text7.style.display = "block";
-    }, 6000)
+    }, 3000)
 }
 
 
