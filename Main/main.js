822c0fea00a8d0ae64b248d57139c530dd4c9de9
Kenneth <knnthgunawan@gmail.com>
2021-11-14 20:17:53 +0800

    Added Google Chart on page 2

--- Main/main.js
+++ Main/main.js
@@ -14,13 +14,46 @@ function choice_2() {
 function choice_3() {
   document.getElementById("1").style.display = "none";
   document.getElementById("2").style.display = "none";
-  document.getElementById("3").style.display = "inline";
-  document.getElementById("4").style.display = "inline";
+  document.getElementById("info").style.display = "block";
   document.getElementById("button_1").style.display = "none";
   document.getElementById("button_2").style.display = "none";
-  document.getElementById("button_3").style.display = "inline";
   setTimeout(function(){
-    document.getElementById("info").style.display = "block";
     document.getElementById("info_p2").style.display = "block";
+    document.getElementById("info_p3").style.display = "block";
+    document.getElementById("reveal").style.display = "block";
   }, 2000)
 }
+
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(drawBasic);
+
+function drawBasic() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'X');
+      data.addColumn('number', 'C02 Emissions');
+
+      data.addRows([
+        [1999, 100],   [2000, 107],  [2001, 118],  [2002, 112],  [2003, 104],  [2004, 116],
+        [2005, 127],  [2006, 140],  [2007, 151],  [2008, 163],  [2009, 177], [2010, 185],
+        [2011, 193],  [2012, 192],  [2013, 191],  [2014, 191],  [2015, 203], [2016, 217],
+        [2017, 229],  [2018, 225],  [2019, 217],  [2020, 211]
+      ]);
+
+      var options = {
+        'width': 350, 'height': 200,
+        hAxis: {
+          title: 'Year'
+        },
+        vAxis: {
+          title: 'C02 Emissions (Million Tonnes)'
+        },
+        legend: {
+          position: 'none'
+        }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+      chart.draw(data, options);
+    }
