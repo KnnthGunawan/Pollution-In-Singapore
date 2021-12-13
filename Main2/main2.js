a0d08739191fcb24547cb5cf6f96a6faf4493882
Kenneth <knnthgunawan@gmail.com>
2021-12-13 21:17:57 +0800

    worked on Page 3 (Added Pie Chart)

---
+++ Main2/main2.js
@@ -0,0 +1,23 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Primary Emission', 'Percentage'],
+    ['Power', 38.9],
+    ['Industry', 46.7],
+    ['Transport', 12.4],
+    ['Others', 1.8]
+  ]);
+
+  var options = {
+    titlePosition: "none",
+    legend: {position: 'labeled'},
+    pieSliceText: "none"
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+  chart.draw(data, options);
+}
